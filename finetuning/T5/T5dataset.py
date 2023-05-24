52910cb8fe001b10046e05ebb4bad68a836bfd7e
matyahiko <sanakan.ver3@gmail.com>
2023-05-24 16:09:43 +0900

    dataset整形コード完了

--- finetuning/T5/T5dataset.py
+++ finetuning/T5/T5dataset.py
@@ -1,12 +1,60 @@
 from torch.utils.data import Dataset, random_split
 
-class T5Dataset(Dataset):
-    def __init__(self, inputs, targets):
-        self.inputs = inputs
-        self.targets = targets
+class TsvDataset(Dataset):
+    def __init__(self, tokenizer, data_dir, type_path, input_max_len=512, target_max_len=512):
+        #self.file_path = os.path.join(data_dir, type_path)
+        self.file_path = data_dir
+        self.input_max_len = input_max_len
+        self.target_max_len = target_max_len
+        self.tokenizer = tokenizer
+        self.inputs = []
+        self.targets = []
 
+        self._build()
+  
     def __len__(self):
         return len(self.inputs)
+  
+    def __getitem__(self, index):
+        source_ids = self.inputs[index]["input_ids"].squeeze()
+        target_ids = self.targets[index]["input_ids"].squeeze()
 
-    def __getitem__(self, idx):
-        return {"input_ids": self.inputs[idx], "labels": self.targets[idx]}
+        source_mask = self.inputs[index]["attention_mask"].squeeze()
+        target_mask = self.targets[index]["attention_mask"].squeeze()
+
+        return {"source_ids": source_ids, "source_mask": source_mask, 
+                "target_ids": target_ids, "target_mask": target_mask}
+
+    def _make_record(self, title, body):
+        # ニュースタイトル生成タスク用の入出力形式に変換する。
+        input = f"{title}"
+        target = f"{body}"
+        return input, target
+  
+    def _build(self):
+        with open(self.file_path, "r", encoding="utf-8") as f:
+            for line in f:
+                line = line.strip().split("\t")
+                assert len(line) == 3
+                assert len(line[0]) > 0
+                assert len(line[1]) > 0
+                #assert len(line[2]) > 0
+
+                title = line[0]
+                body = line[1]
+                #genre_id = line[2]
+
+                input, target = self._make_record(title, body)
+
+                tokenized_inputs = self.tokenizer.batch_encode_plus(
+                    [input], max_length=self.input_max_len, truncation=True, 
+                    padding="max_length", return_tensors="pt"
+                )
+
+                tokenized_targets = self.tokenizer.batch_encode_plus(
+                    [target], max_length=self.target_max_len, truncation=True, 
+                    padding="max_length", return_tensors="pt"
+                )
+
+                self.inputs.append(tokenized_inputs)
+                self.targets.append(tokenized_targets)
--- finetuning/T5/T5fine_simple.py
+++ finetuning/T5/T5fine_simple.py
@@ -1,39 +1,100 @@
-from transformers import T5ForConditionalGeneration, T5Tokenizer, Trainer, TrainingArguments
 import pandas as pd
+import argparse
+import glob
+import os
 import json
-from sklearn.model_selection import train_test_split
-from torch.utils.data import Dataset, random_split
-from 
+import time
+import logging
+import random
+import re
+from itertools import chain
+from string import punctuation
+import numpy as np
+import torch
+from torch.utils.data import Dataset, DataLoader, random_split
+from torch.optim import AdamW
+import pytorch_lightning as pl
 
-# モデルとトークナイザーを準備
-model = T5ForConditionalGeneration.from_pretrained("sonoisa/t5-base-japanese")
-tokenizer = T5Tokenizer.from_pretrained("sonoisa/t5-base-japanese")
+from transformers import (
+    T5ForConditionalGeneration,
+    T5Tokenizer,
+    get_linear_schedule_with_warmup,
+    Trainer
+)
 
-# データの読み込み
-#data = pd.read_csv("../datasets_scripts/corpus/newsplus_input.tsv", sep="\t")
+from T5dataset import TsvDataset
 
-#input_texts = data["input_text"].tolist()
-#target_texts = data["target_text"].tolist()
-             
-with open("../../datasets_scripts/corpus/newsplus_input.json", "r") as f:  
-    data = json.load(f)
+# 乱数シードの設定
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
 
-input_texts = [item['input_text'] for item in data]
-target_texts = [item['target_text'] for item in data]
+set_seed(0)
+
+# GPU利用有無
+USE_GPU = torch.cuda.is_available()
+
+# 各種ハイパーパラメータ
+args_dict = dict(
+    PRETRAINED_MODEL_NAME = "sonoisa/t5-base-japanese"
+    data_dir="../../dataset/2ch_dataset/corpus/newsplus_input.tsv",  # データセットのディレクトリ
+    model_name_or_path=PRETRAINED_MODEL_NAME,
+    tokenizer_name_or_path=PRETRAINED_MODEL_NAME,
+
+    learning_rate=3e-4,
+    betas=(0.9, 0.999),
+    adam_epsilon=1e-8,
+    weight_decay=0.0,
+    warmup_steps=0,
+    gradient_accumulation_steps=1,
+
+    # max_input_length=512,
+    # max_target_length=4,
+    # train_batch_size=8,
+    # eval_batch_size=8,
+    # num_train_epochs=4,
+
+    n_gpu=1 if USE_GPU else 0,
+    early_stop_callback=False,
+    fp_16=False,
+    # opt_level='O1',
+    max_grad_norm=1.0,
+    seed=42,
+)
 
 
-# データのトークナイズ
-inputs = tokenizer(input_texts, truncation=True, padding=True, max_length=512)
-targets = tokenizer(target_texts, truncation=True, padding=True, max_length=512)
 
-# データを学習用と評価用に分割
-train_inputs, val_inputs, train_targets, val_targets = train_test_split(inputs['input_ids'], targets['input_ids'], test_size=0.2)
+         
+# トークナイザー（SentencePiece）モデルの読み込み
+tokenizer = T5Tokenizer.from_pretrained(PRETRAINED_MODEL_NAME, is_fast=True)
 
+# テストデータセットの読み込み
+train_dataset = TsvDataset(tokenizer, args_dict["data_dir"], "test.tsv", 
+                           input_max_len=512, target_max_len=512)
+
+for data in train_dataset:
+    print("A. 入力データの元になる文字列")
+    print(tokenizer.decode(data["source_ids"]))
+    print()
+    print("B. 入力データ（Aの文字列がトークナイズされたトークンID列）")
+    print(data["source_ids"])
+    print()
+    print("C. 出力データの元になる文字列")
+    print(tokenizer.decode(data["target_ids"]))
+    print()
+    print("D. 出力データ（Cの文字列がトークナイズされたトークンID列）")
+    print(data["target_ids"])
+    break
+
+"""
 # 学習の設定
 training_args = TrainingArguments(
     output_dir="./results",
     num_train_epochs=3,
-    per_device_train_batch_size=16,
+    per_device_train_batch_size=16,B
     per_device_eval_batch_size=64,
     warmup_steps=500,
     weight_decay=0.01,
@@ -49,3 +110,4 @@ trainer = Trainer(
 )
 
 trainer.train()
+"""
